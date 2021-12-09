7e36dc67f21ef20f2845224c5726c7f4d99d5490
Dom Vinyard <domvinyard@me.com>
2021-12-09 23:52:10 +0100

    Update schemaList.ts

--- src/blocks/schemaList.ts
+++ src/blocks/schemaList.ts
@@ -10,7 +10,7 @@ export default ({ schema, timestamp }) => {
         type: 'button',
         text: {
           type: 'plain_text',
-          text: '🔎 Database',
+          text: '🔎 View all data',
           emoji: true,
         },
         url: 'https://cloud.hasura.io/project/6a8e17b2-2ea7-446b-bba0-0d7e32b372ae/console',
