f42ec8a6a2d46cbe7ef21d7f36ebe25e528056ab
Dom Vinyard <domvinyard@me.com>
2021-12-09 17:53:33 +0100

    Update schemaList.ts

--- src/blocks/schemaList.ts
+++ src/blocks/schemaList.ts
@@ -3,6 +3,22 @@ export default ({ schema, timestamp }) => {
     {
       type: 'section',
       text: {
+        type: 'mrkdwn',
+        text: `Trainee data`,
+      },
+      accessory: {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: '🔎 View all',
+          emoji: true,
+        },
+        url: 'https://cloud.hasura.io/project/6a8e17b2-2ea7-446b-bba0-0d7e32b372ae/console',
+      },
+    },
+    {
+      type: 'section',
+      text: {
         type: 'plain_text',
         text: "Schema settings. Please don't change these unless you know what you're doing 😅",
         emoji: true,
