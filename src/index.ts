b7626ac265cc11b65b4440517fb4cc2367b5f23c
Dom Vinyard <domvinyard@me.com>
2022-04-08 17:00:43 +0200

    schedule

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 require("dotenv").config();
 import slack from "./clients/slack";
 import listeners from "./listeners";
-import {
-  TimerBasedCronScheduler as scheduler,
-  parseCronExpression,
-} from "cron-schedule";
 import sheets from "./sheets";
+var cron = require("node-cron");
 
 (async () => {
   await slack.start(Number(process.env.PORT) || 5000);
@@ -13,6 +10,8 @@ import sheets from "./sheets";
   console.log("⚡️ CYFBot is listening!");
 
   // Update the sheet every 15 mins
-  const cron = parseCronExpression("*/15 * * * *");
-  scheduler.setInterval(cron, sheets);
+
+  cron.schedule("*/15 * * * *", () => {
+    sheets();
+  });
 })();
