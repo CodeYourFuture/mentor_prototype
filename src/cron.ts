6d300bf31ff9d606353f4b050c7512f644a51286
Dom Vinyard <domvinyard@me.com>
2022-04-08 14:06:32 +0200

    Update cron.ts

--- src/cron.ts
+++ src/cron.ts
@@ -1,7 +1,10 @@
 require("dotenv").config();
-// import slack from './clients/slack';
-// import listeners from './listeners';
 
 (async () => {
+  console.log(process.env);
   console.log("cron");
+  // - List cohorts by listing all of the channels cyfbot is in (except permissions channel)
+  // - Create one spreadsheet per cohort
+  // - Update on cron every 15 mins
+  // - One tab per integration
 })();
