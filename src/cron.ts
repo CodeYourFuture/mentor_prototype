c382427b7c2a31879841c545b2b42b42f826e2be
Dom Vinyard <domvinyard@me.com>
2022-04-08 17:24:43 +0200

    Update cron.ts

--- src/cron.ts
+++ src/cron.ts
@@ -1,10 +1,6 @@
 require("dotenv").config();
 import slack from "./clients/slack";
 import listeners from "./listeners";
-import {
-  TimerBasedCronScheduler as scheduler,
-  parseCronExpression,
-} from "cron-schedule";
 import sheets from "./sheets";
 
 (async () => {
