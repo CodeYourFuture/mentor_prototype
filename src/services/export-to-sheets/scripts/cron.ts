13995e3dd904905ade1299259970ad34bef9f69a
Dom Vinyard <domvinyard@me.com>
2022-04-14 08:20:25 +0000

    Update cron.ts

--- src/services/export-to-sheets/scripts/cron.ts
+++ src/services/export-to-sheets/scripts/cron.ts
@@ -1,6 +1,6 @@
 import sheets from "..";
 (async () => {
   const { RUN_EXPORT_TO_SHEETS_EVERY_MINUTES: mins } = process.env;
-  console.log(`Export-to-sheets: schedule (every ${mins} mins)`);
-  require("node-cron").schedule(`*/${mins} * * * *`, sheets);
+  console.log(`Export-to-sheets: schedule (every ${mins || 30} mins)`);
+  require("node-cron").schedule(`*/${mins || 30} * * * *`, sheets);
 })();
