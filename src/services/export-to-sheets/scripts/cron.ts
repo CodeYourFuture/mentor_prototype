509857784284c053e2f42903c119dd76e2052b90
Dom Vinyard <domvinyard@me.com>
2022-04-16 17:43:17 +0000

    interlace crons

--- src/services/export-to-sheets/scripts/cron.ts
+++ src/services/export-to-sheets/scripts/cron.ts
@@ -1,6 +1,9 @@
 import sheets from "..";
 (async () => {
-  const { RUN_EXPORT_TO_SHEETS_EVERY_MINUTES: mins } = process.env;
-  console.log(`Export-to-sheets: schedule (every ${mins || 30} mins)`);
-  require("node-cron").schedule(`*/${mins || 30} * * * *`, sheets);
+  // const { RUN_EXPORT_TO_SHEETS_EVERY_MINUTES: mins } = process.env;
+  // console.log(`Export-to-sheets: schedule (every ${mins || 30} mins)`);
+  // require("node-cron").schedule(`*/${mins || 30} * * * *`, sheets);
+
+  console.log(`Export-to-sheets: schedule (every even hour)`);
+  require("node-cron").schedule(`0 */2 * * *`, sheets);
 })();
