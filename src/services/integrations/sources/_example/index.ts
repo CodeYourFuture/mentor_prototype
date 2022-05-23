a360455ec8f5d26df0dcbadaa9fe79eb58bba40a
Dom Vinyard <domvinyard@me.com>
2022-05-23 12:15:49 +0100

    Update index.ts

--- src/services/integrations/sources/_example/index.ts
+++ src/services/integrations/sources/_example/index.ts
@@ -5,7 +5,7 @@ import { IntegrationResponse } from "../..";
 // Fetch and return data for a trainee from the external service
 
 export const fetchData = async function (config, id: string) {
-  const key = process.env.api_key; // see default_config.json
+  const key = config.api_key; // see default_config.json
   const response: any = await fetch(`example.io/user=${id}&key=${key}`);
   return response.score;
 };
@@ -17,7 +17,7 @@ export const processData = async function (
   others?: IntegrationResponse[] // Data of other trainees for comparison (optional)
 ): Promise<IntegrationResponse> {
   const scores = others.map((t) => t.score.value);
-  const isTopScore = Math.max(...(scores as number[])) === fetchedData.score;
+  const isTopScore = Math.max(...(scores as number[])) >= fetchedData;
   const sentiment = isTopScore ? "positive" : "neutral";
 
   // This response data is stored with the trainee in JSON format, ready for querying
